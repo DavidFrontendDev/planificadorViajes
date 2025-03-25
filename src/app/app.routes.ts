66c0887178bb3a8269ab8940f24c09f7c51e455c
DavidFrontendDev <davidfrontenddev@gmail.com>
2025-03-25 23:21:11 +0100

    Header terminado responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ViajesComponent } from './viajes/viajes.component';
+import { CalendarioComponent } from './calendario/calendario.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'viajes', component: ViajesComponent },
+  { path: 'calendario', component: CalendarioComponent },
   { path: '**', component: NotFoundComponent },
 ];
